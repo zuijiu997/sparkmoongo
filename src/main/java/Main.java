6270e98da0fe21913bd12184ff433db7f04982ee
zuijiu997 <zuijiu997@163.com>
2019-08-12 17:56:45 +0800

    scope provide

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,19 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.BitSet;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> l = new ArrayList<>(Arrays.asList("1", "2", "3"));
-        String s = JSON.toJSONString(l, SerializerFeature.WRITE_MAP_NULL_FEATURES);
-        System.out.println("11111111: " + s);
+       int a = 10;
+        int a1 = a<<1;
+        BitSet bitSet = new BitSet();
+        System.out.println(bitSet.size());
+        System.out.println(bitSet.get(63));
+
+//        List<String> l = new ArrayList<>(Arrays.asList("1", "2", "3"));
+//        String s = JSON.toJSONString(l, SerializerFeature.WRITE_MAP_NULL_FEATURES);
+//        System.out.println("11111111: " + s);
     }
 }
