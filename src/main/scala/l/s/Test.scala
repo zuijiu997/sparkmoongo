43c499983ce946b940f9d90a2756e5a8885eceee
zuijiu997 <zuijiu997@163.com>
2019-08-12 13:55:56 +0800

    scope provide

--- src/main/scala/l/s/Test.scala
+++ src/main/scala/l/s/Test.scala
@@ -1,15 +1,11 @@
 package l.s
 
-import com.alibaba.fastjson.JSON
-import com.alibaba.fastjson.serializer.{SerializeFilter, SerializerFeature}
-
-import scala.collection.mutable
 import scala.util.control.Breaks
 
 object Test {
     def main(args: Array[String]): Unit = {
         var a = 0
-        val numList = List(1,2,3,4,5,6,7,8,9,10)
+        val numList = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
         val loop = new Breaks
         loop.breakable {
